bb0a786bd8283ebded793e3b7caa5f1545fd8042
Adam Stepniewski <adam.stepniewski.programista@gmail.com>
2023-09-05 19:48:59 +0200

    Updated config.php with data used to connect with database in localhost

--- App/Config.php
+++ App/Config.php
@@ -5,7 +5,7 @@ namespace App;
 /**
  * Application configuration
  *
- * PHP version 7.0
+ * PHP version 7.2.0
  */
 class Config
 {
@@ -14,29 +14,29 @@ class Config
      * Database host
      * @var string
      */
-    const DB_HOST = 'your-database-host';
+    const DB_HOST = 'localhost';
 
     /**
      * Database name
      * @var string
      */
-    const DB_NAME = 'your-database-name';
+    const DB_NAME = 'budget_manager_mvc';
 
     /**
      * Database user
      * @var string
      */
-    const DB_USER = 'your-database-user';
+    const DB_USER = 'bm_mvc';
 
     /**
      * Database password
      * @var string
      */
-    const DB_PASSWORD = 'your-database-password';
+    const DB_PASSWORD = 'G2kIMVmorbNnjb5a';
 
     /**
      * Show or hide error messages on screen
      * @var boolean
      */
-    const SHOW_ERRORS = true;
+    const SHOW_ERRORS = false;
 }

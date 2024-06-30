ac58f66326011bd5b59e9da6826bad521347b2aa
Adam Stepniewski <adam.stepniewski.programista@gmail.com>
2024-06-30 14:57:17 +0200

    Not necessary files deleted

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -24,28 +24,6 @@ class Login extends OpenAccess
     {
         View::renderTemplate('Login/login.html'); 
 
-   //     View::renderTemplate('Test/test.html', [
-   //         'myArray' => []
-   //     ]); 
-
- //    if (preg_match("/^([a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+)* ?[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]+$/","Adam Stępniewski")) {
- //      echo "Works";
- //       }
-  //  "/^[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ]*$/"
-   /*  if (!preg_match("/^([a-zA-Z]+)* ?[a-zA-Z]+$/",$this->username)) {
-        $this->errors[] = "Only letters and one space allowed in name. Please use standard English characters";
-    }  */
-   //  $prev_date = date('Y-m-d', strtotime('2024-05-04' .' -1 day'));
-   //  var_dump(empty(['0']));
-   //     var_dump(preg_match("/^\d+\.\d\d$/","34.345")); 
-    //    var_dump(!empty($_SESSION['flash_notifications']));
-    //    var_dump(\App\Models\CashFlow::returnExpensesId(5));
-     //   View::renderTemplate('Settings/expense_list.html'); 
-     //   var_dump(\App\Models\CashFlow::checkIfCategoryPresentOnExpensesList(5,"traNsport"));
-   //  var_dump(ucfirst(strtolower("dEBit CaRd")));
-
-  // var_dump(empty(\App\Models\CashFlow::returnCategoriesAssignedToUser(5)));
-  //var_dump(empty(false));
     }
 
     /**
